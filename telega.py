c1df7add15d4e0d13ec1e4c9ec12c7fd9c179e80
Fray <kozar20000@mail.ru>
2022-10-22 21:33:03 +0300

    telebot_1.01

--- telega.py
+++ telega.py
@@ -1,5 +1,6 @@
 from telegram import Bot, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler
+from random import randint as rd
 import wikipedia
 
 wikipedia.set_lang('ru')
@@ -13,6 +14,10 @@ dispatcher = updater.dispatcher
 def start(update, context):
     context.bot.send_message(update.effective_chat.id, 'Hello')
 
+def rand(update, context):
+    context.bot.send_message(update.effective_chat.id, f'{rd(1, 100)}')
+
+
 def wiki(update, context):
     text = ' '.join(context.args)
     result = wikipedia.summary(text, sentences=2)
@@ -20,11 +25,13 @@ def wiki(update, context):
 
 
 start_handler = CommandHandler('start', start)
+random_handler = CommandHandler('random', rand)
 wiki_handler = CommandHandler('wiki', wiki)
 
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(random_handler)
 dispatcher.add_handler(wiki_handler)
 
 
 updater.start_polling()
-updater.idle() #ctrl + c
+updater.idle() #ctrl + z
